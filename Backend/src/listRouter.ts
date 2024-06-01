82351f40e39ac59c8e583674036994bcd5338475
Erik <erikpersson0884@gmail.com>
2024-06-02 00:20:09 +0200

    added addList to backend

--- Backend/src/listRouter.ts
+++ Backend/src/listRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import fs from 'fs';
+import { List } from './types/types';
 
 const listFile = './data/lists.json';
 
@@ -8,6 +9,18 @@ const listRouter = Router();
 listRouter.get('/getAllLists', (req, res) => {
   let lists = fs.readFileSync(listFile).toString(); // Convert Buffer to string
   lists = JSON.parse(lists);
+  
+  res.send(lists);
+});
+
+listRouter.post('/addList', (req, res) => {
+  const newList = req.body.newList;
+
+  let lists: string = fs.readFileSync(listFile).toString(); // Convert Buffer to string
+  let parsedLists: List[] = JSON.parse(lists);
+
+  parsedLists.push(newList);
+  fs.writeFileSync(listFile, JSON.stringify(parsedLists));
   
   res.send(lists);
 });
